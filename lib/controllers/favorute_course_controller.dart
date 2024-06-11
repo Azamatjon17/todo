ba013d7849cf5255980e2141084d479578b8d899
Azamatjon17 <153727489+Azamatjon17@users.noreply.github.com>
2024-06-11 15:59:21 +0500

    we

--- lib/controllers/favorute_course_controller.dart
+++ lib/controllers/favorute_course_controller.dart
@@ -5,12 +5,24 @@ import 'package:todo/models/course.dart';
 
 class FavoruteCourseController {
   addFavoriteCourse(Course course, String userId) async {
-    final url = Uri.parse("https://todo-2a867-default-rtdb.firebaseio.com/favourite.json");
+    final url = Uri.parse('https://todo-2a867-default-rtdb.firebaseio.com/favourite.json');
     await http.post(url,
         body: jsonEncode({
           "courseId": course.id,
           "userId": userId,
         }));
+  }
+
+  deleteFavoruite(Course course, String userId) async {
+    final response = await http.get(Uri.parse("https://todo-2a867-default-rtdb.firebaseio.com/favourite.json"));
+    Map<String, dynamic> data = jsonDecode(response.body);
+    String delateItem;
+    data.forEach((key, value) {
+      if (value["courseId"] == course && value["userId"] == userId) {
+        delateItem = key;
+      }
+    });
+    await http.delete(Uri.parse("https://todo-2a867-default-rtdb.firebaseio.com/favourite/$delateItem.json"));
   }
 
   static getFavoriteByUserId(String userId) async {
